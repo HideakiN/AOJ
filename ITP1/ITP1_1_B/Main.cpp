5dc0c8482007a22c098fabde6c0226e349a9dcdf
中石秀昭 <lovely_hide_chan@live.jp>
2018-10-20 18:44:31 +0900

    Modified ITP1_1_B

--- ITP1/ITP1_1_B/Main.cpp
+++ ITP1/ITP1_1_B/Main.cpp
@@ -2,17 +2,17 @@
 * @file Main.cpp
 * @breif １つの整数xを読み込み、xの３乗を計算し出力するプログラム
 * @date 2018/10/20
-* @details 入力として１つの整数xが与えられる。
-*          整数xの３乗の計算結果を１行に出力する。
-*          入力範囲は以下の通り。
-*          　・1 <= x <= 100
+* @details 入力として１つの整数xが与えられる。<br /> 
+* 整数xの３乗の計算結果を１行に出力する。<br />
+* 入力範囲は以下の通り。<br />
+* ・1 <= x <= 100
 */
 #include <cstdio>
 
 using namespace std;
 
 /**
-* @brief ある整数を３乗を計算する
+* @brief ある整数の３乗を計算する
 */
 class XCube
 {
@@ -23,7 +23,7 @@ private:
 public:
     /**
     * @fn bool execute(int x, int& response)
-    * @brief プログラムのコントロール
+    * @brief xの３乗を計算し、計算結果を返す
     * @param[in] x ターゲットデータ
     * @param[out] response 計算結果
     * @return xが範囲内ならtrueを返し、そうでなければfalseを返す
@@ -31,12 +31,13 @@ public:
     bool execute(int x, int& response);
 
 private:
+
     /**
     * @fn int calc(int x, int zan)
-    * @brief ある整数の３乗を計算する
+    * @brief ある整数の階乗を再帰的に計算する
     * @param[in] x ターゲットデータ
     * @param[in] zan 作業量
-    * @return ある整数xのzan乗を返す
+    * @return ある整数xのzan乗
     */
     int calc(int x, int zan);
 };
@@ -57,10 +58,30 @@ bool XCube::execute(int x, int& response)
 
 int XCube::calc(int x, int zan)
 {
-    if (zan <= 1)
-        return x;
+    if (zan <= 0)
+        return 1;
 
     return x * calc(x, zan - 1);
+}
+
+/**
+* @fn void read(int& input)
+* @brief データを標準入力から読み込む
+* @param[out] input 入力データ
+*/
+void read(int& input)
+{
+    scanf("%d", &input);
+}
+
+/**
+* @fn void write(int output)
+* @brief 計算結果を標準出力に書き出す
+* @param[in] output 出力データ
+*/
+void write(int output)
+{
+    printf("%d\n", output);
 }
 
 /**
@@ -72,11 +93,10 @@ int main()
     int input = 0;
     int output = 0;
 
-    scanf("%d", &input);
+    read(input);
 
-    bool rc = XCube().execute(input, output);
-    if (!rc)
+    if (!XCube().execute(input, output))
         return 1;
 
-    printf("%d\n", output);
+    write(output);
 }
